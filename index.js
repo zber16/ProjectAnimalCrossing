cbe1ab8c20c1ae38930297e9520b367d0bd347cf
zber16 <63484141+zber16@users.noreply.github.com>
2020-04-11 00:08:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var cheerio = require('cheerio');
 
 const checkInventory = () => {
     //Paste the url for the product you want
-    const url = "https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255"
+    const url = "https://www.bestbuy.ca/en-ca/product/nintendo-switch-console-with-neon-red-blue-joy-con/13817625"
     //Paste the cookie of your browser when you manually check website with a browser
-    const cookie = "";
+    const cookie = "HSID=AfJ2UbYmLaDKSx6PB; SSID=AseNotzCYggpi1hXK; APISID=7DSeQ3cwVYpamcsH/AaOKlWT80j6iS2iEn; SAPISID=qCkD7xvOWP-MV9a1/AmPH1n-TOxMjIUT3E; __Secure-HSID=AfJ2UbYmLaDKSx6PB; __Secure-SSID=AseNotzCYggpi1hXK; __Secure-APISID=7DSeQ3cwVYpamcsH/AaOKlWT80j6iS2iEn; __Secure-3PAPISID=qCkD7xvOWP-MV9a1/AmPH1n-TOxMjIUT3E; SID=vAcgyqIT1un4L-u-vcVkMQagtsdIl9mEM0rb740Sl6oltQcTplUQsk9l5nHg2o_PjsV3kg.; __Secure-3PSID=vAcgyqIT1un4L-u-vcVkMQagtsdIl9mEM0rb740Sl6oltQcTO5meNFh1buy64oG1zOC67A.; OTZ=5378316_72_76_104100_72_446760; ANID=AHWqTUnnYngWiwRwgMKcAh6TEWqcePWav9dM10OCLCnzV6FzEHosZGiGaonPsgs8; NID=202=j4ZNAjXDfpW8hnckcXuYSQs1jV0UhKPAUzluNWIDtXvU4q8mUsEzIQQpQiKeyuAnpnQ3huzWmbiMIAleKsJSzR6ip_WQ6b15Sk0MlxRLor47mjdqaQbZmGFJtncZN0OgcT2PMRv0m2tPMDKD2jbiINHZHmVMj5Sm-I2lC8ZIyd6LGSYNItZPAq1BAM6p2km3yiaGZNz6S3xox3AlANWvkRO3v46w8MehbAa4_3K0fSPU8VqxUvNlnhjpa_y70PpQ9brD6vAXXWF80Omg5tjGafp6-MveztFpg9MGTpuF5ERMrqKAIKKUdulzXigUN3QtP-aR5LSlgIpaVzcevOZH4fb39oF1TkGedLb6XsyjtO0p; 1P_JAR=2020-04-11-02; SIDCC=AJi4QfFGd3YZhwErT5vfMOHwa3PPb2E_y_cJ6ujyonFNm-Iptfh83LxAqSEnDJCTojP8-yW1tQ
+";
     //Set up in AWS SES following tutorial
-    const sourceEmailAddr = "xyz@gmail.com";
-    const destinationEmailAddrs = ["abc@qq.com"];
+    const sourceEmailAddr = "berryzhang16@gmail.com";
+    const destinationEmailAddrs = ["berryzhang16@gmail.com"];
     
     const req = {
         url: url,
